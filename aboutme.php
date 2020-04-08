8e65d2b8a073efb7e0d89a408cda29b87b6f0e8e
Julian Ruben Benedict <63332243+JulianRubenBenedict@users.noreply.github.com>
2020-04-08 11:48:16 +0700

    Upload aboutme.php

---
+++ aboutme.php
@@ -0,0 +1,6 @@
+<?php 
+    echo "Nama : Julian Ruben Benedict";
+    echo "Kelas : XI RPL 2";
+    echo "Asal Sekolah : SMK Taruna Bhakti Depok";
+    echo "Alamat Rumah : Jl.Pemuda, Perumahan Depok Lama Alam Permai blok B No.2";
+?>
