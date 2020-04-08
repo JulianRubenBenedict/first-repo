eba2d9354f9116f1192495093ba65cacb9a3209c
Julian Ruben Benedict <63332243+JulianRubenBenedict@users.noreply.github.com>
2020-04-08 11:55:26 +0700

    Menghapus alamat dan menambahkan no telp dan email

--- aboutme.php
+++ aboutme.php
@@ -2,5 +2,8 @@
     echo "Nama : Julian Ruben Benedict";
     echo "Kelas : XI RPL 2";
     echo "Asal Sekolah : SMK Taruna Bhakti Depok";
-    echo "Alamat Rumah : Jl.Pemuda, Perumahan Depok Lama Alam Permai blok B No.2";
+?>
+<?php 
+    echo "No. Telp : 081283075707";
+    echo "email : rubenzebuarz3@gmail.com";
 ?>
